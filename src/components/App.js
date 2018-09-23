370289f20489b0d9c4a8824183d9304c33fa879e
Jason Jones <jsjones96@gmail.com>
2018-09-23 08:26:56 -0700

    Add css file

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-
-const GlobalHeader = () => {
-    return <header className="" />;
-};
+import './App.css';
 
 const App = () => {
     return (
         <div>
-            <GlobalHeader />
-            <div style={{ width: '960px', margin: '5rem auto 0' }}>
-                <h1 className="">Hello React Sandbox!</h1>
-            </div>
+            <header>
+                <div>
+                    <h1>Simple React Starter!</h1>
+                </div>
+            </header>
         </div>
     );
 };
